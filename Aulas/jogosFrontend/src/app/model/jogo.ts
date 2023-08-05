3f6c39075940e466682358764ef42d48f7cd3f75
isaac-kapela <kapelajoao4@gmail.com>
2023-08-05 18:16:54 -0300

    lista 7

--- Aulas/jogosFrontend/src/app/model/jogo.ts
+++
@@ -1,9 +0,0 @@
-export class Jogo{
-  public id!: number;
-  public nome!: string;
-  public descricao!: string;
-  public preco!: number;
-  public img!: string;
-  public esta_favoritado!: boolean;
-}
-
