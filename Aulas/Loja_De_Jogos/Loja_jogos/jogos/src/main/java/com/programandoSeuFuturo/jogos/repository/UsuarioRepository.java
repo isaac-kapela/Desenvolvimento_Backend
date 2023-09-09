3907a3d85b1effa59a32b5ded387c59fb587ac4c
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2023-09-09 19:34:28 -0300

    Atualizações

--- Aulas/Loja_De_Jogos/Loja_jogos/jogos/src/main/java/com/programandoSeuFuturo/jogos/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.programandoSeuFuturo.jogos.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-
-}
